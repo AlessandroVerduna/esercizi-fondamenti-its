1db069240afb1cf91d161617a3ce650fe78bfb93
Alessandro Verduna <alessandroverduna@gmail.com>
2026-01-22 13:01:42 +0100

    esercizio in clase fizz buzz

---
+++ lezione_22/01/esercizio_01.py
@@ -0,0 +1,20 @@
+saluto = "Hello virtual machine!"
+
+print(saluto)
+
+#metodi delle stringhe
+print(saluto.capitalize())
+print(saluto.upper())
+print(saluto.lower())
+
+#lunghezza della stringa
+lunghezza = len(saluto)
+
+print(type(lunghezza))
+
+lunghezza = str(lunghezza)
+
+print(type(lunghezza))
+
+print(f"Questa stringa è lunga {lunghezza} caratteri")
+
