1afb468571038fc3634bc9a2574d719d42032d1f
Alessandro Verduna <alessandro.verduna@edu-its.it>
2026-03-06 18:00:53 +0100

    esercizi da fare a casa pitone

---
+++ programmazione_python/primo_1.py
@@ -0,0 +1,31 @@
+"""
+    Autore: Alessandro Verduna
+    Data: 06/03/2026
+
+    Titolo: Primo esercizio
+        Scrivere un programma che legga i coefficienti a e b di un'equazione di primo grado ax=b e
+        ne scriva la soluzione (attenzione al dominio del coefficiente a)
+
+"""
+
+print("Dopo che mi avrai fornito due variabili A e B ti troverò il valore di X nell'equazione: AX = B")
+
+verifica = True
+while verifica == True:
+    try:
+        a,b = map(float,input("Inserisci il valore di A e il valore di B (separati da uno spazio): ").split())
+        if a == 0 and b != 0:
+            print("Numeri validi. Calcolo la risposta\n...\n...\n...")
+            print("Il valore di X è: impossibile")
+            break
+        elif a == 0 and b == 0:
+            print("Numeri validi. Calcolo la risposta\n...\n...\n...")
+            print("Il valore di X è: indeterminato")
+        else:
+            x = b / a
+            print("Numeri validi. Calcolo la risposta\n...\n...\n...")
+            print(f"Il valore di X è: {x}")
+    except:
+        print("Inserisci dei numeri ed inseriscine due separati da uno spazio, altri caratteri non sono accettati")
+    else:
+        verifica = False
---
+++ programmazione_python/primo_2.py
@@ -0,0 +1,13 @@
+"""
+    Autore: Alessandro Verduna
+    Data: 06/03/2026
+
+    Titolo: Secondo esercizio
+        Scrivere un programma che legga i coefficienti a, b e c di un'equazione di secondo grado
+        ax2+bx+c=0 e ne scriva le soluzioni.
+
+"""
+print("Data un espressione AX^2 + BX + C")
+a, b, c = input("Inserisci un valore per A, uno per B e uno per C separati da uno spazio e ti dirò il valore di X: ").split()
+
+x_1 = ()
