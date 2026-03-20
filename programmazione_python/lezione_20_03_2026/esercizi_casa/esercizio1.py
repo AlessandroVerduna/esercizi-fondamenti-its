1061bca4d67a22df562f51d7c77d686a5e63d830
ale.verdu <alessandroverduna@gmail.com>
2026-03-20 21:28:38 +0100

    esercizi python

--- programmazione_python/lezione_20_03_2026/esercizi_casa/esercizio1.py
+++ programmazione_python/lezione_20_03_2026/esercizi_casa/esercizio1.py
@@ -13,24 +13,18 @@ def return_time_in_seconds(ore, minuti, secondi):
     return ore * 3600 + minuti * 60 + secondi
 
 def bigger_time_given(ora_a = 1, minuti_a = 40, secondi_a = 20, ora_b = 2, minuti_b = 50, secondi_b = 11):
-    if ora_a > ora_b:
-        return "Il primo orario fornito è più grande"
-    elif ora_b > ora_a:
-        return "Il secondo orario fornito è più grande"
-    elif minuti_a > minuti_b:
-        return "Il primo orario fornito è più grande"
-    elif minuti_b > minuti_a:
-        return "Il secondo orario fornito è più grande"
-    elif secondi_a > secondi_b:
-        return "Il primo orario fornito è più grande"
-    elif secondi_b > secondi_a:
-        return "Il secondo orario fornito è più grande"
+    totale1 = return_time_in_seconds(ora_a, minuti_a, secondi_a)
+    totale2 = return_time_in_seconds(ora_b, minuti_b, secondi_b)
+
+    if totale1 > totale2:
+        return "Il primo orario è maggiore"
+    elif totale1 == totale2:
+        return "I due orari sono uguali"
     else:
-        return "I due orari sono uguali!"
+        return "Il secondo orario è maggiore"
 
 def validation_input():
-    verificatore = False
-    while verificatore == False:
+    while True:
         input_ore = int(input("Inserisci qui il numero di ore: "))
         input_minuti = int(input("Inserisci qui il numero di minuti: "))
         input_secondi = int(input("Inserisci qui il numero di secondi: "))
@@ -42,28 +36,28 @@ def validation_input():
         elif input_secondi < 0 or input_secondi > 59:
             print("Valore di riferimento dei secondi non valido")
         else:
-            verificatore = True
-    return input_ore, input_minuti, input_secondi 
+            return input_ore, input_minuti, input_secondi 
 
 def main():   
-    # verificatore = False
-    # while verificatore == False:
-    #     input_ore = int(input("Inserisci qui il numero di ore: "))
-    #     input_minuti = int(input("Inserisci qui il numero di minuti: "))
-    #     input_secondi = int(input("Inserisci qui il numero di secondi: "))
-        
-    #     if input_ore < 0:
-    #         print("Valore di riferimento delle ore non valido")
-    #     elif input_minuti < 0 or input_minuti > 59:
-    #         print("Valore di riferimento dei minuti non valido")
-    #     elif input_secondi < 0 or input_secondi > 59:
-    #         print("Valore di riferimento dei secondi non valido")
-    #     else:
-    #         verificatore = True
-    a, b, c = map(int, validation_input())        
-    print(f"L'orario fornito equivale a: {return_time_in_seconds(a, b, c)} secondi")
+    a, b, c = validation_input()
+    totale1 = return_time_in_seconds(a, b, c)        
+    print(f"L'orario fornito equivale a: {totale1} secondi")
 
-    print(bigger_time_given())
+    risposta1 = input("Vuoi inserire un primo orario da comparare? (Y/N) ")
+    risposta2 = input("Vuoi inserire un secondo orariod a comparare? (Y/N) ")
+
+    if risposta1.capitalize() == "Y" and risposta2.capitalize() == "Y":
+        a, b, c = validation_input()
+        d, e, f = validation_input()
+        print(bigger_time_given(a, b, c, d, e, f))
+    elif risposta1.capitalize() == "Y" and risposta2.capitalize() != "Y":
+        a, b, c = validation_input()
+        print(bigger_time_given(a, b, c))
+    elif risposta1.capitalize() != "Y" and risposta2.capitalize() == "Y":
+        d, e, f = validation_input()
+        print(bigger_time_given(ora_b = d, minuti_b = e, secondi_b = f))
+    else:
+        print(bigger_time_given())
 
 if __name__ == "__main__":
     main()
