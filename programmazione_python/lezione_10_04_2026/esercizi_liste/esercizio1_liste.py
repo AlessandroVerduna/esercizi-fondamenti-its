55bae24cfe9280247af1124195f212ab97684617
ale.verdu <alessandroverduna@gmail.com>
2026-04-10 18:18:24 +0200

    esercizi papa

---
+++ programmazione_python/lezione_10_04_2026/esercizi_liste/esercizio1_liste.py
@@ -0,0 +1,11 @@
+"""
+    Autore: Alessandro Verduna
+    Data: 10/04/2026
+
+    Consegna: Primo Esercizio
+        Scrivere un programma python che rimuova gli elementi duplicati da una lista.
+        Esempio:
+        listaIN = [2, -4 ,5,6,5,5,2]
+        listaOUT=[2,-4,5,6]
+"""
+
