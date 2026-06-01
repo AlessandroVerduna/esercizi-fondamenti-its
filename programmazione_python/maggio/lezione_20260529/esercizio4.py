b2c9c5e05a3150156b53c849e32eb0c38a2f9e1d
ale.verdu <alessandroverduna@gmail.com>
2026-06-01 23:33:58 +0200

    esercizi papa finiti

--- programmazione_python/maggio/lezione_20260529/esercizio4.py
+++ programmazione_python/maggio/lezione_20260529/esercizio4.py
@@ -1,118 +1,154 @@
 """
-    Autore: Alessandro Verduna
-    Data: 29/05/2026
+Autore: Alessandro Verduna
+Data: 29/05/2026
 
-    Consegna: Quarto Esercizio
-        Si definisca una classe Persona che abbia i seguenti attributi:
-        ● nome
-        ● indirizzo
-        ● età
-        Tale classe contiene i seguenti metodi: il costruttore, l'overriding del metodo __str__ e tutti i
-        metodi getter e setter degli attributi.
-        Si vogliono derivare dalla classe Persona le seguenti classi:
-        ● Studente
-        ● Lavoratore
-        La prima deve avere gli attributi aggiuntivi:
-        ● Scuola
-        ● Media voti
-        La seconda deve avere gli attributi aggiuntivi:
-        ● Azienda
-        ● Stipendio
-        Aggiungere tutti i metodi getter e setter relativi agli attributi aggiuntivi.
-        Inoltre effettuare l'overriding dei costruttori e del metodo str inserendo gli attributi
-        aggiuntivi.
-        Provare le tre classi instanziando almeno un oggetto per classe e provando qualche
-        metodo.
+Consegna: Quarto Esercizio
+    Si definisca una classe Persona che abbia gli attributi:
+        - nome
+        - indirizzo
+        - età
+    La classe deve contenere:
+        - costruttore
+        - overriding del metodo __str__
+        - getter e setter per tutti gli attributi
+
+    Derivare dalla classe Persona:
+        - Studente (attributi aggiuntivi: scuola, media voti)
+        - Lavoratore (attributi aggiuntivi: azienda, stipendio)
+
+    Aggiungere getter e setter per gli attributi aggiuntivi.
+    Effettuare overriding di costruttori e __str__.
+    Provare le tre classi istanziando almeno un oggetto per classe.
 """
 
 class Persona(object):
+    """
+    Classe base che rappresenta una persona con nome, indirizzo ed età.
+    Gli attributi sono privati e gestiti tramite property.
+    """
+
     def __init__(self, nome, indirizzo, eta):
+        """
+        Costruttore della classe Persona.
+
+        Parametri:
+            nome (str): nome della persona.
+            indirizzo (str): indirizzo di residenza.
+            eta (int): età della persona.
+        """
         self.__nome = nome
         self.__indirizzo = indirizzo
         self.__eta = eta
 
     def __str__(self):
-        return f"Nome: {self.nome}, eta: {self.eta}, indirizzo: {self.indirizzo}"
+        """Restituisce una rappresentazione leggibile dell'oggetto."""
+        return f"Nome: {self.nome}, età: {self.eta}, indirizzo: {self.indirizzo}"
 
+    # Getter
     @property
     def nome(self):
         return self.__nome
-    
+
     @property
     def indirizzo(self):
         return self.__indirizzo
-    
+
     @property
     def eta(self):
         return self.__eta
-    
+
+    # Setter
     @nome.setter
-    def nome(self,val):
+    def nome(self, val):
         self.__nome = val
 
     @indirizzo.setter
-    def indirizzo(self,val):
+    def indirizzo(self, val):
         self.__indirizzo = val
 
     @eta.setter
-    def eta(self,val):
+    def eta(self, val):
         self.__eta = val
-    
+
+
 class Studente(Persona):
+    """
+    Classe derivata che rappresenta uno studente.
+    Aggiunge gli attributi scuola e media voti.
+    """
+
     def __init__(self, nome, indirizzo, eta, scuola, media_voti):
+        """
+        Costruttore ridefinito per aggiungere gli attributi dello studente.
+        """
         super().__init__(nome, indirizzo, eta)
         self.__scuola = scuola
         self.__media_voti = media_voti
 
     def __str__(self):
+        """Restituisce una rappresentazione leggibile dell'oggetto Studente."""
         return f"{super().__str__()}, scuola: {self.__scuola}, media voti: {self.__media_voti}"
-    
+
+    # Getter
     @property
     def scuola(self):
         return self.__scuola
-    
+
     @property
     def media_voti(self):
         return self.__media_voti
-    
+
+    # Setter
     @scuola.setter
-    def scuola(self,val):
+    def scuola(self, val):
         self.__scuola = val
 
     @media_voti.setter
-    def media_voti(self,val):
+    def media_voti(self, val):
         self.__media_voti = val
 
+
 class Lavoratore(Persona):
+    """
+    Classe derivata che rappresenta un lavoratore.
+    Aggiunge gli attributi azienda e stipendio.
+    """
+
     def __init__(self, nome, indirizzo, eta, azienda, stipendio):
+        """
+        Costruttore ridefinito per aggiungere gli attributi del lavoratore.
+        """
         super().__init__(nome, indirizzo, eta)
         self.__azienda = azienda
         self.__stipendio = stipendio
 
     def __str__(self):
+        """Restituisce una rappresentazione leggibile dell'oggetto Lavoratore."""
         return f"{super().__str__()}, azienda: {self.__azienda}, stipendio: {self.__stipendio}"
 
+    # Getter
     @property
     def azienda(self):
         return self.__azienda
-    
+
     @property
     def stipendio(self):
         return self.__stipendio
-    
+
+    # Setter
     @azienda.setter
-    def azienda(self,val):
+    def azienda(self, val):
         self.__azienda = val
 
     @stipendio.setter
-    def stipendio(self,val):
+    def stipendio(self, val):
         self.__stipendio = val
 
 persona1 = Persona("Giada", "Cumiana", 17)
-studente1 = Studente("Marco", "Piossasco", 85, "Airasca", 10)
-lavoratore1 = Lavoratore("Teresa", "Pinerolo", 12, "Bau INC", 10000)
+studente1 = Studente("Marco", "Piossasco", 18, "Airasca", 10)
+lavoratore1 = Lavoratore("Teresa", "Pinerolo", 32, "Bau INC", 10000)
 
-lavoratore2 = Lavoratore(persona1.nome, persona1.eta, persona1.indirizzo, "Miao INC", 50506)
+lavoratore2 = Lavoratore(persona1.nome, persona1.indirizzo, persona1.eta, "Miao INC", 50506)
 
 print(persona1)
 print(studente1)
--- programmazione_python/maggio/lezione_20260529/esercizio5.py
+++ programmazione_python/maggio/lezione_20260529/esercizio5.py
@@ -1,69 +1,110 @@
 """
-    Autore: Alessandro Verduna
-    Data: 29/05/2026
+Autore: Alessandro Verduna
+Data: 29/05/2026
 
-    Consegna: Quinto Esercizio
-        Creare una classe AritmeticaDue con attributi operando1 e operando2. Definire il
-        costruttore utilizzando parametri con valori predefiniti e il metodo str.
-        Aggiungere due metodi uno che restituisca la differenza e l'altro il prodotto dei due
-        operandi. Implementare un terzo metodo che permetta il confronto tra il risultato del
-        prodotto di due oggetti AritmeticaDue (in sostanza indicare se il prodotto è maggiore di
-        quello calcolato nell'oggetto AritmeticaDue passato come parametro).
+Consegna: Quinto Esercizio
+    Creare una classe AritmeticaDue con attributi operando1 e operando2. Definire il
+    costruttore utilizzando parametri con valori predefiniti e il metodo str.
+    Aggiungere due metodi: uno che restituisca la differenza e l'altro il prodotto dei due
+    operandi. Implementare un terzo metodo che permetta il confronto tra il risultato del
+    prodotto di due oggetti AritmeticaDue.
 
-        Derivare dalla classe AritmeticaDue la classe AritmeticaTre aggiungendo l'attributo
-        operando3. Ridefinire il costruttore, il metodo str e i tre metodi differenza, prodotto e
-        confronto. Aggiungere un metodo per il calcolo della somma di tutti gli attributi.
-        Provare le classi e i metodi implementati.
+    Derivare dalla classe AritmeticaDue la classe AritmeticaTre aggiungendo l'attributo
+    operando3. Ridefinire costruttore, metodo str, differenza, prodotto e confronto.
+    Aggiungere un metodo per il calcolo della somma di tutti gli attributi.
+    Provare le classi e i metodi implementati.
 """
 
 class AritmeticaDue(object):
-    def __init__(self, operando1, operando2):
+    """
+    Classe che rappresenta operazioni aritmetiche con due operandi.
+    Gli attributi sono privati e accessibili tramite property.
+    """
+
+    def __init__(self, operando1=0, operando2=0):
+        """
+        Costruttore con parametri predefiniti.
+
+        Parametri:
+            operando1 (int/float): primo operando.
+            operando2 (int/float): secondo operando.
+        """
         self.__operando1 = operando1
         self.__operando2 = operando2
 
     def __str__(self):
+        """Restituisce una rappresentazione leggibile dell'oggetto."""
         return f"Operando 1: {self.__operando1}, operando 2: {self.__operando2}"
 
+    # Property per accedere agli attributi privati
+    @property
+    def operando1(self):
+        return self.__operando1
+
+    @property
+    def operando2(self):
+        return self.__operando2
+
     def differenza(self):
+        """Restituisce la differenza tra i due operandi."""
         return self.__operando1 - self.__operando2
-    
+
     def prodotto(self):
+        """Restituisce il prodotto dei due operandi."""
         return self.__operando1 * self.__operando2
-    
-    def confronto(self, val: object):
-        if self.prodotto() > val.prodotto():
-            return True
-        else:
-            return False
-        
+
+    def confronto(self, val):
+        """
+        Confronta il prodotto dell'oggetto chiamante con quello dell'oggetto passato.
+
+        Parametri:
+            val (AritmeticaDue): oggetto da confrontare.
+
+        Ritorna:
+            bool: True se il prodotto dell'oggetto chiamante è maggiore.
+        """
+        return self.prodotto() > val.prodotto()
+
+
 class AritmeticaTre(AritmeticaDue):
-    def __init__(self, operando1, operando2, operando3):
+    """
+    Classe derivata che aggiunge un terzo operando e ridefinisce i metodi.
+    """
+
+    def __init__(self, operando1=0, operando2=0, operando3=0):
+        """
+        Costruttore ridefinito con tre operandi.
+        """
         super().__init__(operando1, operando2)
         self.__operando3 = operando3
 
     def __str__(self):
+        """Restituisce una rappresentazione leggibile dell'oggetto."""
         return f"{super().__str__()}, operando 3: {self.__operando3}"
-    
+
     def differenza(self):
+        """Restituisce la differenza tra i tre operandi."""
         return super().differenza() - self.__operando3
 
     def prodotto(self):
+        """Restituisce il prodotto dei tre operandi."""
         return super().prodotto() * self.__operando3
-    
-    def confronto(self, val: object):
-        if self.prodotto() > val.prodotto():
-            return True
-        else:
-            return False
-        
-    def somma(self):
-        return self.__operando1 + self.__operando2 + self.__operando3
-    
-numeri1 = AritmeticaDue(10,8)
-numeri2 = AritmeticaDue(10,50)
 
-numeri3 = AritmeticaTre(10,5,2)
-numeri4 = AritmeticaTre(1,111,4)
+    def confronto(self, val):
+        """Confronta il prodotto dell'oggetto chiamante con quello dell'altro oggetto."""
+        return self.prodotto() > val.prodotto()
+
+    def somma(self):
+        """Restituisce la somma dei tre operandi."""
+        return self.operando1 + self.operando2 + self.__operando3
+
+
+# Test delle classi come richiesto dalla consegna
+numeri1 = AritmeticaDue(10, 8)
+numeri2 = AritmeticaDue(10, 50)
+
+numeri3 = AritmeticaTre(10, 5, 2)
+numeri4 = AritmeticaTre(1, 111, 4)
 
 print(numeri1.differenza())
 print(numeri1.prodotto())
